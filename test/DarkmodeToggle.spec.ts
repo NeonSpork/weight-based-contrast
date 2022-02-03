43ee3b0a239b3970df000f72b70e07bcff24e734
NeonSpork <leiknes@gmail.com>
2022-02-03 09:48:59 +0100

    chore: config for testing

---
+++ test/DarkmodeToggle.spec.ts
@@ -0,0 +1,7 @@
+import { cleanup, fireEvent, render, screen } from '@testing-library/svelte';
+import DarkmodeToggle from '$lib/components/DarkmodeToggle.svelte';
+import { theme } from '$lib/store'
+
+describe('DarmkodeToggle.svelte', () => {
+  afterEach(() => cleanup())
+})
